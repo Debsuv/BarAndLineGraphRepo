31f858a38c2027a19f2fa7e9521be8e7916efa2b
Debanjan Chakraborty <debanjan.chakraborty@randem.co.in>
2015-01-20 16:05:20 +0530

    Initial Commit

---
+++ CustomBarSection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomBarSection
+//
+//  Created by Debanjan Chakraborty on 20/01/15.
+//  Copyright (c) 2015 RandemIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomBarSection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomBarSection
+//
+//  Created by Debanjan Chakraborty on 20/01/15.
+//  Copyright (c) 2015 RandemIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
